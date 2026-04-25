a05c0b5a7005e27a960a5f75fd2521894a44dad8
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-04-24 23:39:15 -0300

    feat: added bones registry

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { Header, Hero, Button, Section, Card, Grid } from './components'
 import './App.css'
-import './src/bones/registry'
+import './bones/registry'
 
 function App() {
   const [count, setCount] = useState(0)
